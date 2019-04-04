1183f124ad03b51340a1850fdc2893cb0faecf6a
schoi369 <schoi369@gatech.edu>
2019-04-04 16:44:06 -0400

    Add files via upload

--- app/src/main/java/com/example/m6/model/TechLevel.java
+++ app/src/main/java/com/example/m6/model/TechLevel.java
@@ -1,12 +1,47 @@
 package com.example.m6.model;
 
+/**
+ * Technology levels that can be used
+ */
 public enum TechLevel {
+
+    /**
+     * Pre Agriculture
+     */
     PRE_AGRICULTURE,
+
+    /**
+     * Agriculture
+     */
     AGRICULTURE,
+
+    /**
+     * Medieval
+     */
     MEDIEVAL,
+
+    /**
+     * Renaissance
+     */
     RENAISSANCE,
+
+    /**
+     * Early Industrial
+     */
     EARLY_INDUSTRIAL,
+
+    /**
+     * Industrial
+     */
     INDUSTRIAL,
+
+    /**
+     * Post Industrial
+     */
     POST_INDUSTRIAL,
-    HI_TECH;
+
+    /**
+     * Hi Tech
+     */
+    HI_TECH
 }
