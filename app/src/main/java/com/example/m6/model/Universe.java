ae27bcf45d81308d089623c151113392753755d6
schoi369 <schoi369@gatech.edu>
2019-04-04 16:44:56 -0400

    Add files via upload

--- app/src/main/java/com/example/m6/model/Universe.java
+++ app/src/main/java/com/example/m6/model/Universe.java
@@ -2,36 +2,50 @@ package com.example.m6.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@SuppressWarnings("ALL")
+/** Represents a Universe
+ * @author Juntae Kim
+ * @author Woonglae Cho
+ * @author Jinhyung Park
+ * @author Yeonjoo Yoo
+ * @author Semin Choi
+ */
 public class Universe implements Serializable {
 
-    //Acamar is initial solar system when new player is created.
-    String[] nameList = {"Acamar", "Baratas", "Calondia", "Daled", "Exo", "Ferris", "Hades",
-            "Kira", "Lave", "Kimchi"};
-
-    boolean[][] coordinate = new boolean[150][100];
     private List<SolarSystem> system = new ArrayList<>();
+
+    /**
+     *
+     */
     public Universe(){
         int systemCount = 0;
         while(systemCount < 10) {
-            int x = (int) (Math.random() * 150);
-            int y = (int) (Math.random() * 100);
+            final int universeWidth = 150;
+            int x = (int) (Math.random() * universeWidth);
+            final int universeLength = 100;
+            int y = (int) (Math.random() * universeLength);
+            boolean[][] coordinate = new boolean[universeWidth][universeLength];
             if(!coordinate[x][y]) {
+                final int multiplyNum = 12;
+                //Acamar is initial solar system when new player is created.
+                String[] nameList = {"Acamar", "Baratas", "Calondia", "Daled", "Exo",
+                        "Ferris", "Hades", "Kira", "Lave", "Kimchi"};
                 system.add(new SolarSystem(nameList[systemCount], x, y, (int)(Math.random()*7),
-                        (int)(Math.random()*12)));
+                        (int)(Math.random()* multiplyNum)));
                 coordinate[x][y]=true;
                 systemCount++;
             }
         }
     }
 
+    /**
+     *
+     * @return List of Solar Systems
+     */
     public List<SolarSystem> getSystem() {
-        return system;
-    }
-    public boolean[][] getCoordinate(){
-        return coordinate;
+        return Collections.unmodifiableList(system);
     }
 
 }
